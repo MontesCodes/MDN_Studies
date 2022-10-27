274772652c4787f46310cd8c2a2e22525e2d19a4
MontiesCodes_1557 <MontiesWebDev@Gmail.com>
2022-10-27 08:15:41 -0400

    updating Number guessing game start, BRLT1557 10-27-2022

--- number guessing game start/js/script.js
+++ number guessing game start/js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowWrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
